e65359ef33492c6017846fa43f64b39bb6f5c2df
liaimv <liaimv0301@gmail.com>
2025-04-03 11:46:50 -0400

    Add game success management script

--- Ice Movement Game/Assets/1/Scripts/IceMelt.cs
+++ Ice Movement Game/Assets/1/Scripts/IceMelt.cs
@@ -1,3 +1,4 @@
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class IceMelt : MonoBehaviour
@@ -16,26 +17,52 @@ public class IceMelt : MonoBehaviour
 
     private PlayerController controllerScript;
 
+    public Canvas gameOverCanvas;
+    public Canvas gameWinCanvas;
+
+    private float originalPlayerSpeed;
+    private float originalJumpForce;
+    private float originalRotationSpeed;
+
+    private Vector3 originalPosition;
+    private Vector3 checkpointPosition;
+
     void Start()
     {
         controllerScript = GetComponentInParent<PlayerController>();
         originalScale = transform.localScale.x;
         meltSpeed = OGmeltSpeed;
+
+        originalPlayerSpeed = controllerScript.playerSpeed;
+        originalJumpForce = controllerScript.jumpForce;
+        originalRotationSpeed = controllerScript.rotationSpeed;
+
+        originalPosition = transform.position;
+        checkpointPosition = originalPosition;
+
+        gameOverCanvas.gameObject.SetActive(false);
+        gameWinCanvas.gameObject.SetActive(false);
     }
 
     void FixedUpdate()
     {
         if (transform.localScale.x > 0.05f)
         {
-            meltTimer += Time.deltaTime * meltSpeed * meltTimerMultiplier;
+            meltTimer += Time.deltaTime * meltSpeed * meltTimerMultiplier; //Unity doesn't like small numbers, so I multiply the meltSpeed with the meltTimerMultiplier
             meltAmount = meltCurve.Evaluate(meltTimer);
             MeltIceCube();
+
+            gameOverCanvas.gameObject.SetActive(false);
         }
         else
         {
             controllerScript.playerSpeed = 0;
             controllerScript.jumpForce = 0;
             controllerScript.rotationSpeed = 0;
+
+            gameOverCanvas.gameObject.SetActive(true);
+
+            transform.position = checkpointPosition;
         }
     }
 
@@ -43,9 +70,9 @@ public class IceMelt : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Cold"))
         {
-            meltSpeed = meltSlowSpeed; 
+            meltSpeed = meltSlowSpeed;
         }
-        
+
         if (collision.gameObject.CompareTag("Hot"))
         {
             meltSpeed = meltFastSpeed;
@@ -57,11 +84,52 @@ public class IceMelt : MonoBehaviour
         {
             meltSpeed = OGmeltSpeed;
         }
-        
+
         if (collision.gameObject.CompareTag("Hot"))
         {
             meltSpeed = OGmeltSpeed;
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Stage2"))
+        {
+            checkpointPosition = transform.position;
+            ResetSize();
+        }
+        if (other.gameObject.CompareTag("Stage3"))
+        {
+            checkpointPosition = transform.position;
+            ResetSize();
+        }
+        if (other.gameObject.CompareTag("Stage4"))
+        {
+            checkpointPosition = transform.position;
+            ResetSize();
+        }
+
+        if (other.gameObject.CompareTag("Win"))
+        {
+            gameWinCanvas.gameObject.SetActive(true);
+            ResetSize();
+        }
+    }
+
+    public void ResetGame()
+    {
+
+        ResetSize();
+        controllerScript.playerSpeed = originalPlayerSpeed;
+        controllerScript.jumpForce = originalJumpForce;
+        controllerScript.rotationSpeed = originalRotationSpeed;
+    }
+
+    private void ResetSize()
+    {
+        meltAmount = 0f;
+        meltTimer = 0f;
+        transform.localScale = new Vector3(originalScale, originalScale, originalScale);
     }
 
     void MeltIceCube()
