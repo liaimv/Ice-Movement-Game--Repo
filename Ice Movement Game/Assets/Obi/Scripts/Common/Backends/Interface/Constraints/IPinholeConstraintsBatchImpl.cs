55f71f7037a89f0465e6ffc73acbc14bb9368c50
hyunju12345 <hyunju03yoo@gmail.com>
2025-04-04 09:08:58 -0400

    remove obi

--- Ice Movement Game/Assets/Obi/Scripts/Common/Backends/Interface/Constraints/IPinholeConstraintsBatchImpl.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace Obi
-{
-    public interface IPinholeConstraintsBatchImpl : IConstraintsBatchImpl
-    {
-        void SetPinholeConstraints(ObiNativeIntList particleIndices, ObiNativeIntList colliderIndices, ObiNativeVector4List offsets, ObiNativeFloatList edgeMus, ObiNativeIntList edgeRanges, ObiNativeFloatList edgeRangeMus, ObiNativeFloatList parameters, ObiNativeFloatList relativeVelocities, ObiNativeFloatList lambdas, int count);
-    }
-}
